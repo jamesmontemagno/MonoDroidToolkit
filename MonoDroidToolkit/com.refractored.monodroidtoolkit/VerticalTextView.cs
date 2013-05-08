2125fcc0064bfc5c2e25d5b09b22961df725dbb3
James Montemagno <james.montemagno@gmail.com>
2013-05-08 11:04:34 -0700

    Add where ported from

--- MonoDroidToolkit/com.refractored.monodroidtoolkit/VerticalTextView.cs
+++ MonoDroidToolkit/com.refractored.monodroidtoolkit/VerticalTextView.cs
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Ported from mic: http://stackoverflow.com/users/907695/mice
+ * via: http://stackoverflow.com/questions/1258275/vertical-rotated-label-in-android
  */
 
 using Android.Content;
