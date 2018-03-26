b540c82ad3b8908761f6efd845e55f3be6f590a2
James Montemagno <james.montemagno@gmail.com>
2018-03-26 10:19:59 -0700

    Remove outdated info

--- src/XamarinAndroidToolkit/Toolkit/ScaleImageView.cs
+++ src/XamarinAndroidToolkit/Toolkit/ScaleImageView.cs
@@ -1,20 +1,4 @@
-/*
- * Copyright (C) 2013 @JamesMontemagno http://www.montemagno.com http://www.refractored.com
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * Ported from mic: https://github.com/matabii/scale-imageview-android/blob/master/src/com/matabii/dev/scaleimageview/ScaleImageView.java
- */
+
 
 using System;
 
--- src/XamarinAndroidToolkit/Toolkit/VerticalTextView.cs
+++ src/XamarinAndroidToolkit/Toolkit/VerticalTextView.cs
@@ -1,21 +1,4 @@
-/*
- * Copyright (C) 2013 @JamesMontemagno http://www.montemagno.com http://www.refractored.com
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * Ported from mic: http://stackoverflow.com/users/907695/mice
- * via: http://stackoverflow.com/questions/1258275/vertical-rotated-label-in-android
- */
+
 
 using Android.Content;
 using Android.Graphics;
